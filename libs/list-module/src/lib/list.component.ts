0c5a4124b1e52ab5f315e579eb5b916e00f9b9c8
kreuzerk <kevin.kreuzer90@icloud.com>
2022-05-07 23:56:34 +0200

    feat: 🎸 (filters) create the filters skeleton

--- libs/list-module/src/lib/list.component.ts
+++ libs/list-module/src/lib/list.component.ts
@@ -8,12 +8,45 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'list',
   template: `
+    <strong>Filters</strong>
+
+    <div class="filters">
+      <mat-form-field>
+        <mat-label>Name</mat-label>
+        <input matInput />
+      </mat-form-field>
+      <mat-form-field>
+        <mat-label>Base class</mat-label>
+        <input matInput />
+      </mat-form-field>
+      <mat-form-field>
+        <mat-label>Second class</mat-label>
+        <input matInput />
+      </mat-form-field>
+      <mat-slide-toggle>Played</mat-slide-toggle>
+    </div>
+
     <list-item
       [classCombinations]="classCombinations$ | async"
       (playClass)="play($event)"
     >
     </list-item>
   `,
+  styles: [
+    `
+      .filters {
+        display: flex;
+        gap: 20px;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 30px;
+      }
+
+      * {
+        flex: 1;
+      }
+    `,
+  ],
 })
 export class ListComponent implements OnInit {
   classCombinations$: Observable<ClassCombination[]> | undefined;
--- libs/list-module/src/lib/list.module.ts
+++ libs/list-module/src/lib/list.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { ListComponent } from './list.component';
 import { PlayedPipe } from './played.pipe';
 import { ListItemComponent } from './list-item.component';
 
 @NgModule({
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSlideToggleModule,
+  ],
   declarations: [ListComponent, ListItemComponent, PlayedPipe],
   exports: [ListComponent, ListItemComponent, PlayedPipe],
 })
