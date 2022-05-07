54aaee96b179fee085cf6b08b934b257d6b27538
kreuzerk <kevin.kreuzer90@icloud.com>
2022-05-07 23:23:28 +0200

    feat: 🎸 (list) display list

--- libs/list-module/src/lib/list.component.ts
+++ libs/list-module/src/lib/list.component.ts
@@ -8,11 +8,49 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'list',
   template: `
-    <pre>
-   {{ classCombinations$ | async | json }}
-   </pre
-    >
+    <div class="list-heading">
+      <strong>Name</strong>
+      <strong>Base class</strong>
+      <strong>Second class</strong>
+      <strong class="align-right">played</strong>
+    </div>
+    <mat-card *ngFor="let classCombination of classCombinations$ | async">
+      <span>{{ classCombination.name }}</span>
+      <span>{{ classCombination.baseClass }}</span>
+      <span>{{ classCombination.secondClass }}</span>
+      <!-- TODO use the pipe -->
+      <span class="align-right">{{ classCombination.played }}</span>
+    </mat-card>
   `,
+  styles: [
+    `
+      .list-heading {
+        display: flex;
+        justify-content: space-between;
+        padding-left: 16px;
+        padding-right: 16px;
+        margin-bottom: 20px;
+
+        strong {
+          flex: 1;
+        }
+      }
+
+      mat-card {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 20px;
+
+        span {
+          flex: 1;
+        }
+      }
+
+      .align-right {
+        text-align: right;
+      }
+    `,
+  ],
 })
 export class ListComponent implements OnInit {
   classCombinations$: Observable<ClassCombination[]> | undefined;
