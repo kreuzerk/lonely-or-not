c49c502dfab5d16f9219d43cfb55cb68472a8ec9
kreuzerk <kevin.kreuzer90@icloud.com>
2022-05-08 13:30:30 +0200

    feat: 🎸 (home) add home component

--- apps/modules/src/app/app.module.ts
+++ apps/modules/src/app/app.module.ts
@@ -3,13 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HeaderModule } from '@lonely-or-not/header-module';
+import { HomeModule } from '@lonely-or-not/home-module';
 import { ListModule } from '@lonely-or-not/list-module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, HeaderModule, ListModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HeaderModule,
+    ListModule,
+    HomeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
