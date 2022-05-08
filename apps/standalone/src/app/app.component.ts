e6a0854c9643f31c7d68c6ff2ea9320ea6cdcf89
kreuzerk <kevin.kreuzer90@icloud.com>
2022-05-08 08:35:34 +0200

    feat: 🎸 (standalone) add standalone component

---
+++ apps/standalone/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lonely-or-not-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+})
+export class AppComponent {
+  title = 'standalone';
+}
